e0fa25847bee0186539eae19372169218cf95f4e
rakhagi <rahmadkhamari@gmail.com>
2022-11-13 14:10:05 +0700

    upload pertama

---
+++ Desktop/My Porto/script.js
@@ -0,0 +1,11 @@
+//SCRIPT UNTUK NAVBAR
+
+function myFunction() {
+    var x = document.getElementById("navigation");
+
+    if (x.style.display === "flex") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "flex";
+    }
+  }
